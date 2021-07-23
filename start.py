58f4d501f42eeaac68c5ec24da87ed9954411740
bigtiddygirl <bigtiddygirl@yandex.ru>
2021-07-23 11:51:50 +0500

    fix time

--- start.py
+++ start.py
@@ -82,7 +82,7 @@ async def scheduled(wait_for):
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
-	loop.create_task(scheduled(9))
+	loop.create_task(scheduled(1800))
 	executor.start_polling(dp, skip_updates=True)
 
 
